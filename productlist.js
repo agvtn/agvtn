cd95ae55a588fce0120f1b98a0cb8fbe90525672
Agnes Sofie Vatnhamar <agne4440@stud.kea.dk>
2021-08-31 14:03:04 +0200

    adding img to productlistjs

--- productlist.js
+++ productlist.js
@@ -1,4 +1,8 @@
-const url = "https://kea-alt-del.dk/t7/api/products";
+//From category site to productlist site
+
+const urlParams = new URLSearchParams(window.location.search);
+const season = urlParams.get("season");
+const url = "https://kea-alt-del.dk/t7/api/products?season=" + season;
 
 fetch(url)
   .then(function (res) {
@@ -16,20 +20,27 @@ function showProduct(product) {
   console.log(product);
   //soldOut onSale
   //grab template
+  console.log(product);
   const template = document.querySelector("#smallProductTemplate").content;
   //clone it
   const copy = template.cloneNode(true);
+
   //change content
+  copy.querySelector(
+    "img"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  copy.querySelector("a").setAttribute("href", "product.html?id=" + product.id);
+
   copy.querySelector(
     ".subtle"
   ).textContent = `${product.articletype} | ${product.brandname}`;
   copy.querySelector("h3").textContent = product.productdisplayname;
 
   if (product.soldout) {
-    copy.querySelector("article").classlist.add("soldOut");
+    copy.querySelector("article").classList.add("soldOut");
   }
   if (product.discount) {
-    copy.querySelector("article").classlist.add("onSale");
+    copy.querySelector("article").classList.add("onSale");
 
     copy.querySelector(".discounted p").textContent =
       product.price / product.discount;
